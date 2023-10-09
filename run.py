e5464f4cf0f3dbcdd3c4a7b039f483109986653c
Richard Marles <marleswebinfo@gmail.com>
2023-10-09 10:07:42 +0200

    feat : adding player_choose function select the position on the game zone and change for X

--- run.py
+++ run.py
@@ -6,15 +6,17 @@ game_zone = [" · "," · "," · ",
              " · "," · "," · ",
              " · "," · "," · ",]
 
-def print_game_zone(game_zone):
+runin_game = True
+
+def print_game_zone(patron):
     """
     print the game board in to the terminal
     """
-    print (game_zone[0] + "|" + game_zone[1] + "|" + game_zone[2] )
+    print (patron[0] + "|" + patron[1] + "|" + patron[2] )
     print ("___________")
-    print (game_zone[3] + "|" + game_zone[4] + "|" + game_zone[5] )
+    print (patron[3] + "|" + patron[4] + "|" + patron[5] )
     print ("___________")
-    print (game_zone[6] + "|" + game_zone[7] + "|" + game_zone[8] )
+    print (patron[6] + "|" + patron[7] + "|" + patron[8] )
     
 # start the game with message asking name player
 def start_game():
@@ -26,14 +28,33 @@ def start_game():
     print ("The first person to make a row with their three pieces wins\n ")
     player_name = input("Please Enter your Name ")
 
+    print ("Hi "+ player_name + " Please choose a number from 1 to 9")
+    print ("the board starts from the upper left corner\n ")
+
+    return player_name
 
 # take player input
+def player_choose(patron):
+    """
+    We put the player choose in a var and compare
+    if a right number o it's not already used
+    """       
+    position = int(input("please select your number (1-9)"))
+    print (position)
+
+    if position >= 1 and position <= 9 and patron[position - 1] == " · ":
+        patron[position-1] = "X"
+    else:
+        print ("The Number in not correct or that position is used")
+
+
 
 #check for win or tie
 
 # switch the player
 
 # check for win or tie agaion
-
-start_game()
-print_game_zone(game_zone)
+user = start_game()
+while runin_game:
+    print_game_zone(game_zone)
+    player_choose(game_zone)
